970213bae13d16057b64862bcba0b4c2e8fc8744
Michal Nedzka <michael.nedzka@gmail.com>
2018-11-17 22:35:39 +0100

    Install Enzyme and enzyme-adapter and configure them

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,3 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "../App";
+
+it("shows a comment box", () => {
+  const div = document.createElement("div");
+
+  ReactDOM.render(<App />, div);
+
+  expect(div.innerHTML).toContain("commentBox");
+
+  ReactDOM.unmountComponentAtNode(div);
+});
