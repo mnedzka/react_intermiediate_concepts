116e0c989c076ab70d236f22a2f653ae80e09cf8
Michal Nedzka <michael.nedzka@gmail.com>
2018-11-17 23:52:34 +0100

    Change paths for relative, change commentBox component from functional to class

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default () => {
-  return <div>commentBox</div>;
-};
+export default class CommentBox extends Component {
+  state = {
+    comment: ""
+  };
+
+  handleChange = event => {
+    this.setState({ comment: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({ comment: "" });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a comment</h4>
+        <textarea onChange={this.handleChange} value={this.state.comment} />
+        <div>
+          <button>Submit comment</button>
+        </div>
+      </form>
+    );
+  }
+}
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,14 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { shallow } from "enzyme";
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
 
-import App from "../App";
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
 
 it("shows a comment box", () => {
-  const div = document.createElement("div");
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
 
-  ReactDOM.render(<App />, div);
-
-  expect(div.innerHTML).toContain("commentBox");
-
-  ReactDOM.unmountComponentAtNode(div);
+it("shows a comment ;ist", () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
