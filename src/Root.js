c41dc38c0ca15a2e0bb458a958fa3c70e3ee5bb9
Michal Nedzka <michael.nedzka@gmail.com>
2018-11-19 23:37:00 +0100

    Apply middlewareand fetch comments from api in actions

--- src/Root.js
+++ src/Root.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import reducers from "reducers";
+import reduxPromise from "redux-promise";
 
 export default ({ children, initialState = {} }) => {
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(reduxPromise)
+  );
   return (
     <div>
-      <Provider store={createStore(reducers, initialState)}>
-        {children}
-      </Provider>
+      <Provider store={store}>{children}</Provider>
     </div>
   );
 };
