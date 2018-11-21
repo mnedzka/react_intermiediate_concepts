ef2a1a59bde010ffef3ed9116b1b527ddc76c570
Michal Nedzka <michael.nedzka@gmail.com>
2018-11-21 23:39:13 +0100

    Add react-router and modify app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import CommentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
+import { Route, Link } from "react-router-dom";
 
-export default function App() {
-  return (
-    <div>
-      <CommentBox />
-      <CommentList />
-    </div>
-  );
+export default class App extends Component {
+  renderButton() {}
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post a omment</Link>
+        </li>
+        <li>{this.render.button}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
 }
