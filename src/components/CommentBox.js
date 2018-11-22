6308c2011612aeb091ca9133f493035ab0834e4a
Michal Nedzka <michael.nedzka@gmail.com>
2018-11-22 23:06:47 +0100

    Move methods, pass props and connect comment box to requireAuth

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,25 +1,12 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "actions";
+import requireAuth from "components/requireAuth";
 
 class CommentBox extends Component {
   state = {
     comment: ""
   };
-
-  componentDidMount() {
-    this.shouldLeave();
-  }
-
-  componentDidUpdate() {
-    this.shouldLeave();
-  }
-
-  shouldLeave() {
-    if (!this.props.auth) {
-      this.props.history.push("/");
-    }
-  }
 
   handleChange = event => {
     this.setState({ comment: event.target.value });
@@ -50,13 +37,7 @@ class CommentBox extends Component {
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    auth: state.auth
-  };
-}
-
 export default connect(
-  mapStateToProps,
+  null,
   actions
-)(CommentBox);
+)(requireAuth(CommentBox));
--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 export default ChildComponent => {
   class ComposedComponent extends Component {
+    componentDidMount() {
+      this.shouldLeave();
+    }
+
+    componentDidUpdate() {
+      this.shouldLeave();
+    }
+
+    shouldLeave() {
+      if (!this.props.auth) {
+        this.props.history.push("/");
+      }
+    }
+
     render() {
-      return <ChildComponent />;
+      return <ChildComponent {...this.props} />;
     }
   }
 
-  return ComposedComponent;
+  function mapStateToProps(state) {
+    return {
+      auth: state.auth
+    };
+  }
+
+  return connect(mapStateToProps)(ComposedComponent);
 };
